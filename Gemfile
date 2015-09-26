8e1b89565b8e8ee6d9caf2c273e85fd27c6e5823
amanuelg <ama.ghya@gmail.com>
2015-09-26 22:26:27 +0000

    added image uploader

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3.5'
 gem 'sass-rails', '>= 3.2'
+gem 'carrierwave', '0.10.0'
+gem 'mini_magick', '3.8.0'
 
 group :development, :test do
   gem 'byebug'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,5 +6,16 @@ class Recipe < ActiveRecord::Base
     validates :summary, presence: true, length: {minimum: 10 ,maximum: 150}
     validates :description, presence: true, length: {minimum: 20 ,maximum: 500}
     
+    mount_uploader :picture, PictureUploader
+    
+    validate :picture_size
+    
+    
+    private
+        def picture_size
+            if picture.size > 2.megabytes
+                errors.add(:picture, "should be less than 5MB")
+            end
+        end
     
 end
