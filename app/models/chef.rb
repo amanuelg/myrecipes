ac222bcea0bf09fef1a4566ac4c63ac6b1c2172e
amanuelg <ama.ghya@gmail.com>
2016-01-21 14:16:23 +0000

    handling image uploading in production

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,8 +5,7 @@ class Chef < ActiveRecord::Base
     before_save {self.email = self.email.downcase}
     validates :chef_name, presence: true, length: {minimum: 3, maximum:40}
     VALID_EMAIL_REGX =  /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length:{maximum:105},
-    uniqueness:{case_sensitive: false},
+    validates :email, presence: true, length:{maximum:105},   uniqueness:{case_sensitive: false},
     format: {with: VALID_EMAIL_REGX}
     
     has_secure_password
